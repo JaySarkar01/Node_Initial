27153e3f36162398ab33c136370c0dfa77fed95c
Jay <jaisarkari9@gmail.com>
2024-12-05 18:06:23 +0530

    put api

--- api.js
+++ api.js
@@ -2,10 +2,28 @@ const express = require("express");
 const dbConnect = require("./mongodb");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", async (req, resp) => {
   let data = await dbConnect();
   data = await data.find().toArray();
   resp.send(data);
 });
+
+app.post('/',async(req,resp)=>{
+    let data = await dbConnect();
+    let result = await data.insertOne(req.body);
+    resp.send(result);
+})
+
+app.put("/:name", async (req,resp)=>
+{
+    let data = await dbConnect();
+    let result = data.updateOne(
+        {name:req.params.name},
+        {$set: req.body}
+    )
+    resp.send({result:"update"})
+})
 
 app.listen(5000);
